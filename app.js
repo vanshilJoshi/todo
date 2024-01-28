b3e203d6ddb507dd001f227f811099e1830b9718
vanshilJoshi <156924117+vanshilJoshi@users.noreply.github.com>
2024-01-28 17:46:39 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+let btn=document.querySelector("button");
+let ul=document.querySelector("ul");
+let inp=document.querySelector("input");
+
+btn.addEventListener("click",function(){
+    let item=document.createElement("li");
+    item.innerText=inp.value;
+
+    let delBtn=document.createElement("button");
+    delBtn.innerText="Done";
+    delBtn.classList.add("Done");
+
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    inp.value="";
+});
+
+
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listItem=event.target.parentElement;
+        listItem.remove();
+        console.log("Done");
+    }
+})
